964bff75a99dd9063542bf7a859acc250fc85eca
student <student@kas-it21-pc41>
2022-11-17 10:26:01 +0900

    HomeServlet作成

---
+++ src/main/java/HomeServlet.java
@@ -0,0 +1,13 @@
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
