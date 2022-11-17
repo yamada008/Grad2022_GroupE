1385b7962e0ed5ddc30b673584a42da526d83052
kannnomiina <ksw2170285@stu.o-hara.ac.jp>
2022-11-17 10:44:46 +0900

    bootstrap導入

--- src/main/java/HomeServlet.java
+++ src/main/java/HomeServlet.java
@@ -1,13 +1,25 @@
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/HomeServlet")
+@WebServlet("/")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+
+		req.getRequestDispatcher("WEB-INF/jsp/home.jsp").forward(req, resp);
+	}
 }
